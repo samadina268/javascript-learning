67e2a53ccaae9d8e7ef84eca2227e8958288f43d
samadina268 <abubakarsamad26@gmail.com>
2025-10-04 18:02:53 +0100

    third commit

--- dina.js
+++ dina.js
@@ -1,78 +1,83 @@
 // number
 let age = 32;
-let price= 4000;
+let price = 4000;
 
 // string
-let name ="seyi";
-let greeting ="Good evening, Everyone";
+let name = "seyi";
+let greeting = "Good evening, Everyone";
 
 // booleans
 let isClassOnline = true;
 let hasArrived = false;
 
 // null
-let car= null;
+let car = null;
 
 // undefined
 let address;
 
 // object
-let mobilePhone ={
-    name:"samsung", 
-    releaseYear: 2024,
-    isNew: false,
-}
-
+let mobilePhone = {
+  name: "samsung",
+  releaseYear: 2024,
+  isNew: false,
+};
 
 // BigInts
 let bigNum = 123456789012345678901234567890n;
-let y=bigNum
+let y = bigNum;
 
 console.log("bigNum", bigNum);
 console.log("y", y);
 
-console.log("age", ContentVisibilityAutoStateChangeEvent)
+console.log("age", ContentVisibilityAutoStateChangeEvent);
 
+console.log("check for age", age === 32);
 
-
-console.log("check for age", age===32);
-
-
-// todo 
+// todo
 
 // ass-1
-let myName="Samadina";
+let myName = "Samadina";
 let hobby = "coding";
 console.log("myName", myName);
-console.log('hobby', hobby);
+console.log("hobby", hobby);
 
 // ass-2
-let a=40;
-let b=40;
+let a = 40;
+let b = 40;
 
-let sum= a+b;
-let difference =a-b;
-let product = a*b;
-let division = a/b;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let division = a / b;
 
+console.log("sum", sum);
+console.log("difference", difference);
+console.log("product", product);
+console.log("division", division);
 
+let activity = "playingCodm";
 
-console.log('sum', sum);
-console.log('difference', difference);
-console.log('product', product);
-console.log('division', division);
+console.log("activity", activity);
 
+// modula this is used to get the remainder
+20 / 3;
 
-let activity="playingCodm";
+const myConcantenation = "I am" + " " + "Full";
 
-console.log('activity', activity)
+console.log(myConcantenation);
 
+const myObject = {
+  title: "javascript Fundamentals",
+  yearOfPublication: 1996,
+  yearOfRelease: 2019,
+};
 
+console.log(myObject);
 
+myObject.myType ="frictional";
 
-
-
-
+console.log(myObject);
 
 
 
